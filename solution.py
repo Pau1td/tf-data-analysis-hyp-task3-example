b504f49884421db6eb0beddcd772cf3ac66f3cc1
Pau1td <110543968+Pau1td@users.noreply.github.com>
2023-04-12 23:50:31 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,24 @@
 import pandas as pd
 import numpy as np
-
+from statsmodels.stats.proportion import proportions_ztest
 
 chat_id = 278913153 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    #return ... # Ваш ответ, True или False  
+
+    pval_tz = 0.03
+
+    count = np.sum(x)
+    nobs = np.sum(y)
+    stat, pval = proportions_ztest(count, nobs, alternative='smaller')
+    #print('{0:0.3f}'.format(pval))
+
+    if pval <= pval_tz:
+      return True
+    else:
+      return False
